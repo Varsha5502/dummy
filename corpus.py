d0f9b1cc6d76f229b01cdc0562147bb6a2ec4386
Varsha5502 <155927703+Varsha5502@users.noreply.github.com>
2024-03-06 16:07:24 -0500

    Create corpus.py

---
+++ corpus.py
@@ -0,0 +1,6 @@
+
+def count_token_occurrences(file_path, token):
+    with open(file_path, 'r') as file:
+        corpus_text = file.read()
+        count = corpus_text.lower().count(token.lower())
+    return count
